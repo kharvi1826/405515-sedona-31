162cc987be1236919a1445b780bffe22b552e020
kharvi1826 <84926543+kharvi1826@users.noreply.github.com>
2021-07-15 16:20:53 +0200

    Оживляем интерактивные элементы

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const searchButton = document.querySelector("search-button2");
+let  popup = document.querySelector("booking");
+const form = document.querySelector("booking-hotel");
+const data = form.querySelector("date-field");
+
+popup.classList.add("popup-close");
+
+searchButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.toggle("popup-show");
+  popup.classList.toggle("popup-close");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!data.value || !people.value) {
+    evt.preventDefault();
+    popup.classList.add("error");
+  }
+});
